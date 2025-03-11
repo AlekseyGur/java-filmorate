a2ccfe03164a1aca7a98470e94b89726a3cc80c2
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-03-11 16:34:37 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -10,25 +10,25 @@ import ru.yandex.practicum.filmorate.storage.MpaStorage;
 @Service
 public class MpaService {
 
-    private final MpaStorage MpaStorage;
+    private final MpaStorage mpaStorage;
 
-    public MpaService(MpaStorage MpaStorage) {
-        this.MpaStorage = MpaStorage;
+    public MpaService(MpaStorage mpaStorage) {
+        this.mpaStorage = mpaStorage;
     }
     
     public Mpa get(Long id) {
-        return MpaStorage.getMpa(id);
+        return mpaStorage.getMpa(id);
     }
     
     public Mpa create(Mpa Mpa) {
-        return MpaStorage.addMpa(Mpa);
+        return mpaStorage.addMpa(Mpa);
     }
 
     public void remove(Long id) {
-        MpaStorage.removeMpa(id);
+        mpaStorage.removeMpa(id);
     }
 
     public List<Mpa> findAll() {
-        return MpaStorage.findAll();
+        return mpaStorage.findAll();
     }
 }
