5ff7cbd197433fcfd28c485702d616a06fbc0e1c
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-04-05 14:28:01 +0300

    Codestyle

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
